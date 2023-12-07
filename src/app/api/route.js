546bc03c2655211130840616ff60cc05befe43fb
Arthur <arthurzinugf@gmail.com>
2023-12-07 11:27:22 -0300

    fix: fixed env

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,23 +1,20 @@
 const winston = require("winston");
 require("winston-daily-rotate-file");
-const { combine, timestamp, json, colorize, align, printf } = winston.format;
+const { combine, timestamp, json, colorize } = winston.format;
 
 const { Logtail } = require("@logtail/node");
 const { LogtailTransport } = require("@logtail/winston");
 
-const logtail = new Logtail(process.env.SOURCE_TOKEN);
-
+const logtail = new Logtail(`${process.env.SOURCE_TOKEN}`);
+console.log(logtail)
 
 const logger = winston.createLogger({
-  level: process.env.LOG_LEVEL || 'info',
+  level: `${process.env.LOG_LEVEL}` || 'info',
   format: combine(
   colorize({all:true}),
   timestamp({
     format: 'DD-MM-YYYY hh:mm:ss.SSS A',
-  }), 
-  align(),
-  printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
-  ),
+  }), json()),
   transports: [
     new LogtailTransport(logtail),
   ],
