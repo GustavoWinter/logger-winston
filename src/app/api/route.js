e4964d63b964b19dea682b8b1e35678dcd2b4a4c
Arthur <arthurzinugf@gmail.com>
2023-12-08 10:16:50 -0300

    fix: file route

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -4,13 +4,8 @@ const { combine, timestamp, json, colorize } = winston.format;
 
 const logger = winston.createLogger({
   level: `${process.env.LOG_LEVEL}` || 'info',
-  format: combine(
-  colorize({all:true}),
-  timestamp({
-    format: 'DD-MM-YYYY hh:mm:ss.SSS A',
-  }), json()),
   transports: [
-    new winston.transports.Console({ stderrLevels: ['error']}),
+    new winston.transports.Console(),
   ],
 });
 
