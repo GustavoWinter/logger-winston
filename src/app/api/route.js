6a2fa3f6ba100bc8b3d856c3241ea0022340f20c
Arthur <arthurzinugf@gmail.com>
2023-12-08 11:59:49 -0300

    fix: remove log file

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -4,13 +4,12 @@ const { combine, timestamp, json, colorize } = winston.format;
 
 export const logger = winston.createLogger({
   level: `${process.env.LOG_LEVEL}` || 'info',
+  format: combine(timestamp(), json()),
   transports: [
-    new winston.transports.Console(),
-    new winston.transports.File({
-      filename: 'logs.log'
-    })
+    new winston.transports.Console()
   ],
 });
+logger.error('DEU ERRO');
 
 
 export async function GET() {
